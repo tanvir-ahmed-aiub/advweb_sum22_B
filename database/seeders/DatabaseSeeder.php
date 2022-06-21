fccd3195f370588c4bfa5873a8037b75670588d5
Tanvir <tanvir.ahmeed.aiub@gmail.com>
2022-06-21 13:52:10 +0600

    all

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+use App\Models\UMSStudent;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        for($i=0;$i<200;$i++){
+        DB::table('students_info')->insert([
+            'name' => Str::random(10),
+            'user_id' => Str::random(10),
+            'username' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+            'dob'=>"12.12.12",
+            'cgpa'=>2.35,
+            'd_id'=>1,
+            'pro_pic'=>""
+        ]);
+    }
         // \App\Models\User::factory(10)->create();
     }
 }
